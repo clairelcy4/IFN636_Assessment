4b627f6959f6a454840220035bc0ff79abde725a
徐煥強 <N11445203@qut.edu.au>
2025-10-02 19:53:58 +1000

    renew.Pet.js

--- backend/models/Pet.js
+++ backend/models/Pet.js
@@ -1,22 +1,46 @@
 const mongoose = require("mongoose");
 
-const petSchema = new mongoose.Schema(
+
+const animalSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     species: { type: String },
     breed: { type: String },
     age: { type: Number },
     allergyMed: { type: String },
-    ownerName: { type: String },
-    ownerPhone: { type: String },
-    ownerEmail: { type: String },
-    ownerId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    discriminatorKey: "kind", 
+  }
 );
 
-module.exports = mongoose.model("Pet", petSchema);
+
+animalSchema.methods.getInfo = function () {
+  return `Name: ${this.name}, Species: ${this.species}`;
+};
+
+
+const Animal = mongoose.model("Animal", animalSchema);
+
+
+const petSchema = new mongoose.Schema({
+  ownerName: { type: String },
+  ownerPhone: { type: String },
+  ownerEmail: { type: String },
+  ownerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+
+petSchema.methods.getInfo = function () {
+  return `Name: ${this.name}, Species: ${this.species}, Owner: ${this.ownerName}`;
+};
+
+
+const Pet = Animal.discriminator("Pet", petSchema);
+
+module.exports = { Animal, Pet };
